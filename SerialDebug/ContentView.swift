5c7e2d78167a6a087ee457a4a54cb356d74b9a0b
Lucy He <bitlucyhe@icloud.com>
2025-07-06 15:23:49 +0800

    Initial Commit

---
+++ SerialDebug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SerialDebug
+//
+//  Created by 何秋洋 on 2025/7/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SerialDebug/SerialDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerialDebugApp.swift
+//  SerialDebug
+//
+//  Created by 何秋洋 on 2025/7/6.
+//
+
+import SwiftUI
+
+@main
+struct SerialDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
